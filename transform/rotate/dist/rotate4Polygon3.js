6d1eb66bc05f7136a1579d56291dd76ac19f09c3
ShingoHosoda <tomas0ark@gmail.com>
2021-03-07 11:50:53 +0900

    update

---
+++ transform/rotate/dist/rotate4Polygon3.js
@@ -0,0 +1,109 @@
+import { Vector2 } from "./mathematics/vector.js";
+import { Mathematics } from "./mathematics/mathematics.js";
+import { Line } from "./geomety/line.js";
+import { Grid } from "./utility/grid.js";
+import { Rectangle } from "./geomety/rectangle.js";
+import { Polygon } from "./geomety/polygon.js";
+const canvas = document.querySelector("canvas");
+if (!(canvas instanceof HTMLCanvasElement)) {
+    throw new Error("not found canvas element");
+}
+const gl = canvas.getContext("2d");
+if (!gl) {
+    throw new Error("canvasの初期化に失敗しました。");
+}
+let width = 0;
+let height = 0;
+let angle;
+/// ここでは簡単な正三角形を扱う。
+let triangle_location1 = [];
+let triangle_size1;
+const init = () => {
+    canvas.width = width = window.innerWidth;
+    canvas.height = height = window.innerHeight;
+    angle = 0;
+    set_triangle();
+};
+let renderGrid = () => {
+    let line_location1_1 = new Vector2(width / 4.0, 0.0);
+    let line_location1_2 = new Vector2(width / 4.0, height);
+    let line1 = new Line(gl, line_location1_1, line_location1_2);
+    let line_location2_1 = new Vector2(0.0, height / 2.0);
+    let line_location2_2 = new Vector2(width, height / 2.0);
+    let line2 = new Line(gl, line_location2_1, line_location2_2);
+    let line_location3_1 = new Vector2(width / 2.0, 0.0);
+    let line_location3_2 = new Vector2(width / 2.0, height);
+    let line3 = new Line(gl, line_location3_1, line_location3_2);
+    let line_location4_1 = new Vector2(width - width / 4.0, 0.0);
+    let line_location4_2 = new Vector2(width - width / 4.0, height);
+    let line4 = new Line(gl, line_location4_1, line_location4_2);
+    let line_data = [line2, line1, line3, line4];
+    Grid.gridN(line_data, "#000000");
+};
+/// 0 ~ 1のみを基準にする
+const set_triangle = () => {
+    const TRIANGLE_MAX = 3;
+    let x1 = [0, 0.5, 1];
+    let y1 = [1, 0, 1];
+    /// 三角形なので大きさは100(すべて同じ)
+    triangle_size1 = 200;
+    for (let count = 0; count < TRIANGLE_MAX; count++) {
+        triangle_location1[count] = new Vector2(x1[count] * triangle_size1, y1[count] * triangle_size1);
+        console.log(triangle_location1[count].x, triangle_location1[count].y);
+    }
+};
+const background = (color) => {
+    const CANVAS_LOCATION = new Vector2(0, 0);
+    const CANVAS_SIZE = new Vector2(width, height);
+    let setBackground = new Rectangle(gl, CANVAS_LOCATION, CANVAS_SIZE);
+    setBackground.draw_fill(color);
+};
+/// これは弧度法それとも度数法? -> 弧度法
+const renderRotate = () => {
+    angle += 0.05;
+    /// 三角形の描画準備
+    let triangle = new Polygon(gl, triangle_location1);
+    const SHIFT = triangle_size1 * 0.5;
+    /// N°の回転
+    // 多角形(三角形)の?を軸とした回転
+    gl.save();
+    gl.translate(width / 4.0 - SHIFT, height / 2.0 - SHIFT);
+    triangle.drawPolygon2("rgba(240, 255, 240, 0.5)");
+    gl.restore();
+    /// ?軸とした回転
+    gl.save();
+    gl.translate(width / 4.0 - SHIFT, height / 2.0 - SHIFT);
+    gl.rotate(Mathematics.degTorad(angle));
+    triangle.drawPolygon2("rgba(240, 255, 240, 0.8)");
+    gl.restore();
+    /// N°の回転
+    // 多角形(三角形)の?を軸とした回転
+    gl.save();
+    gl.translate(width - width / 4.0 - SHIFT, height / 2.0 - SHIFT);
+    triangle.drawPolygon2("rgba(240, 255, 240, 0.5)");
+    gl.restore();
+    /// ?軸とした回転
+    gl.save();
+    gl.translate(width - width / 4.0 - SHIFT, height / 2.0 - SHIFT);
+    gl.translate(SHIFT, SHIFT);
+    gl.rotate(Mathematics.degTorad(angle));
+    gl.translate(-SHIFT, -SHIFT);
+    triangle.drawPolygon2("rgba(240, 255, 240, 0.8)");
+    gl.restore();
+};
+const main = () => {
+    background("rgb(190, 205, 215)");
+    // const WIDTH = canvas.width;
+    // const HEIGHT = canvas.height;
+    // const WIDTH2 = WIDTH / 2.0;
+    // const HEIGHT2 = HEIGHT / 2.0;
+    // const WINDOW_SIZE = new Vector2(WIDTH, HEIGHT);
+    /// 基準となるグリッドを描く
+    gl.save();
+    renderGrid();
+    gl.restore();
+    renderRotate();
+    requestAnimationFrame(main);
+};
+init();
+window.onload = main;
